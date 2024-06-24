9d737d175a6384491348a93ad9ccff5085a2fa62
MuhammadMoftah <muhammad.moftah3787@gmail.com>
2024-06-24 18:03:20 +0300

    animations

--- components/Layout/Sidebar.vue
+++ components/Layout/Sidebar.vue
@@ -5,10 +5,11 @@
     @mouseleave="collapseBtn = false"
   >
     <LayoutSidebarNavigations class="w-full" />
+
     <!-- collapse button  -->
-    <transition name="navbtn">
+    <transition name="fade">
       <button
-        class="absolute top-0 bottom-0 z-10 h-16 p-1 m-auto rounded-r-none cursor-pointer w-7 text-emerald-50 btn -left-7 bg-theme1 hover:bg-theme1 hover:contrast-125"
+        class="absolute top-0 bottom-0 z-10 h-16 p-1 m-auto rounded-r-none cursor-pointer btn w-7 text-emerald-50 -left-7 bg-theme1 hover:bg-theme1 hover:contrast-125"
         @click="
           useGlobalStore().sideCollapsed = !useGlobalStore().sideCollapsed
         "
@@ -35,7 +36,7 @@ const sideCollapsed = computed(() => useGlobalStore().sideCollapsed);
     transform 0.2s cubic-bezier(0.5, 0, 0.7, 0.4);
   transition-delay: calc(0.2s * (var(--total) - var(--i)));
 
-  @apply rtl:translate-x-4 ltr:-translate-x-4;
+  @apply translate-x-4;
 }
 
 .navbtn-leave-to {
@@ -43,6 +44,16 @@ const sideCollapsed = computed(() => useGlobalStore().sideCollapsed);
     transform 0.2s cubic-bezier(0.5, 0, 0.7, 0.4);
   transition-delay: calc(0.2s * (var(--total) - var(--i)));
 
-  @apply rtl:translate-x-4 ltr:-translate-x-4;
+  @apply translate-x-4;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
