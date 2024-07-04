112bb5c19946bc1077d43fbba4b86b7b04b1e43a
MuhammadMoftah07 <muhammad.moftah.3787@gmail.com>
2024-07-04 22:27:30 +0300

    edit auth 3

--- middleware/authenticated.ts
+++ middleware/authenticated.ts
@@ -1,17 +1,15 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  return;
   const { status } = useAuth();
   if (status.value == "unauthenticated") {
+    // if (process.client) {
+    //   useNuxtApp().$toast.info(ThemeLoginToast, { multiple: false });
+    // }
     nextTick(() => {
-      // await useRouter().push("/auth/login");
       // This will only be executed on the client side
       useNuxtApp().$toast.info("من فضلك سجل دخول", {
         autoClose: 2000,
       });
-      // return navigateTo("/auth/login");
     });
-
-    // return abortNavigation();
-    return navigateTo("/auth/login");
+    return abortNavigation();
   }
 });
